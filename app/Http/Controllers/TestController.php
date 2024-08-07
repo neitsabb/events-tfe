80bb68e360b6aa459fbe5b95746dca35d9e68b67
BastienVdp <bastienpp@hotmail.com>
2024-08-07 19:40:27 +0200

    build: setup Laravel app with Breeze, Inertia, React, TS, Tailwind

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class TestController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return Inertia::render('Test', [
+            'message' => "Hello, World!",
+        ]);
+    }
+}
---
+++ resources/js/Pages/Test.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+    return <div className="text-red-400">Test</div>;
+};
+
+export default Test;
