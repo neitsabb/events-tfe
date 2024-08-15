a76648c8a7a1819258c7b8a929294382bf37c0f3
BastienVdp <bastienpp@hotmail.com>
2024-08-16 00:07:24 +0200

    refactor: remove user_id and use organization_id, shared organizations data

--- app/Events/Admin/Http/Controllers/ShowEventSingleController.php
+++ app/Events/Admin/Http/Controllers/ShowEventSingleController.php
@@ -6,6 +6,8 @@ namespace App\Events\Admin\Http\Controllers;
 use App\Events\Shared\Models\Event;
 use App\Events\Shared\Resources\EventResource;
 use App\Shared\Http\Controller;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class ShowEventSingleController extends Controller
@@ -13,7 +15,7 @@ class ShowEventSingleController extends Controller
 	/**
 	 * Display a listing of the resource.
 	 */
-	public function __invoke($id, $panel = 'overview', $subpanel = 'general'): \Inertia\Response
+	public function __invoke($id, $panel = 'overview', $subpanel = 'general'): mixed
 	{
 		$panels = ['overview', 'sales', 'tickets', 'settings'];
 
@@ -23,11 +25,16 @@ class ShowEventSingleController extends Controller
 			$subpanels = ['general', 'preferences', 'advanced'];
 			if (!in_array($subpanel, $subpanels)) abort(404);
 		}
-		return Inertia::render(
-			isset($subpanels) ? 'Events/Admin/Show/Settings/' . ucfirst($subpanel) . '/View' : 'Events/Admin/Show/' . ucfirst($panel) . '/View',
-			[
-				'event' => new EventResource(Event::where('id', $id)->firstOrFail()),
-			]
-		);
+
+		if (Gate::inspect('view', Event::where('id', $id)->firstOrFail())->allowed()) {
+			return Inertia::render(
+				isset($subpanels) ? 'Events/Admin/Show/Settings/' . ucfirst($subpanel) . '/View' : 'Events/Admin/Show/' . ucfirst($panel) . '/View',
+				[
+					'event' => new EventResource(Event::where('id', $id)->firstOrFail()),
+				]
+			);
+		}
+
+		return Redirect::route('events.index');
 	}
 }
