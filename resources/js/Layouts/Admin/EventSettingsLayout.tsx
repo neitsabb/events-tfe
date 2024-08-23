29f4cce53c86ddb567587f9d8562791a05b73b48
BastienVdp <bastienpp@hotmail.com>
2024-08-23 21:54:40 +0200

    refactor: settings layout reusable component

--- resources/js/Layouts/Admin/EventSettingsLayout.tsx
+++ resources/js/Layouts/Admin/EventSettingsLayout.tsx
@@ -1,103 +1,43 @@
-import { buttonVariants } from '@/Components/ui/button';
 import { Event } from '@/types';
-import { cn } from '@/utils';
 import React, { PropsWithChildren } from 'react';
 import EventSingleLayout from './EventSingleLayout';
-import { Separator } from '@/Components/ui/separator';
-import { Link } from '@inertiajs/react';
-
-const sidebarNavItems = [
-  {
-    title: 'Paramètres généraux',
-    subpanel: 'general',
-  },
-  {
-    title: 'Préférences',
-    subpanel: 'preferences',
-  },
-  {
-    title: 'Zone de danger',
-    subpanel: 'advanced',
-  },
-];
-
-interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
-  items: {
-    subpanel: string;
-    title: string;
-  }[];
-  event: Event;
-}
+import { SettingsLayout } from './SettingsLayout';
 
 interface EventSettingsLayoutProps {
   event: Event;
 }
 
+const eventSidebarItems = [
+  { title: 'Paramètres généraux', subpanel: 'general' },
+  { title: 'Préférences', subpanel: 'preferences' },
+  { title: 'Zone de danger', subpanel: 'advanced' },
+];
+
+export interface SidebarNavItem {
+  subpanel?: string; // Optionnel pour l'organisation
+  panel?: string;
+  title: string;
+}
+
 export const EventSettingsLayout: React.FC<
   PropsWithChildren<EventSettingsLayoutProps>
 > = ({ event, children }) => {
+  const getEventRoute = (item: SidebarNavItem) => {
+    const subpanelPath =
+      item.subpanel && item.subpanel !== 'general' ? `/${item.subpanel}` : '';
+    return `/dashboard/events/${event.id}/settings${subpanelPath}`;
+  };
+
   return (
     <EventSingleLayout event={event}>
-      <div className="space-y-6 ">
-        <div className="space-y-0.5">
-          <h2 className="text-xl font-bold tracking-tight">Configuration</h2>
-          <p className="text-muted-foreground text-sm">
-            Vous pouvez personnaliser les paramètres de votre événement ici.
-          </p>
-        </div>
-        <Separator className="my-6" />
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-6 lg:space-y-0">
-          <aside className="lg:w-1/5 !top-0">
-            <SidebarSettings items={sidebarNavItems} event={event} />
-          </aside>
-          <div className="flex-1">{children}</div>
-        </div>
-      </div>
+      <SettingsLayout
+        title="Paramètres de l'événement"
+        description="Gérez les paramètres de votre événement ici."
+        sidebarNavItems={eventSidebarItems}
+        getRoute={getEventRoute}
+      >
+        {children}
+      </SettingsLayout>
     </EventSingleLayout>
-  );
-};
-
-const SidebarSettings = ({
-  className,
-  items,
-  event,
-  ...props
-}: SidebarNavProps) => {
-  return (
-    <nav
-      className={cn(
-        'flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1',
-        className
-      )}
-      {...props}
-    >
-      {items.map((item) => {
-        const location = window.location.pathname.split('/').pop();
-        const isActive =
-          location !== 'settings'
-            ? location === item.subpanel
-            : item.subpanel === 'general';
-        return (
-          <Link
-            key={item.subpanel}
-            href={route('events.show', {
-              id: event.id,
-              panel: 'settings',
-              subpanel: item.subpanel !== 'general' ? item.subpanel : '',
-            })}
-            className={cn(
-              buttonVariants({ variant: 'ghost' }),
-              {
-                'bg-muted hover:bg-muted': isActive,
-                'hover:bg-transparent hover:underline': !isActive,
-              },
-              'justify-start'
-            )}
-          >
-            {item.title}
-          </Link>
-        );
-      })}
-    </nav>
   );
 };
---
+++ resources/js/Layouts/Admin/SettingsLayout.tsx
@@ -0,0 +1,39 @@
+import { SidebarSettings } from '@/Components/Admin/SidebarSettings';
+import { Separator } from '@/Components/ui/separator';
+import React, { PropsWithChildren } from 'react';
+
+interface SettingsLayoutProps {
+  title?: string;
+  description?: string;
+  sidebarNavItems: any[];
+  getRoute: (item: any) => string;
+  isActive?: (item: any) => boolean;
+}
+
+export const SettingsLayout: React.FC<
+  PropsWithChildren<SettingsLayoutProps>
+> = ({ title, description, sidebarNavItems, getRoute, isActive, children }) => {
+  return (
+    <div className="space-y-6">
+      {title && description ? (
+        <>
+          <div className="space-y-0.5 mt-6">
+            <h2 className="text-xl font-bold tracking-tight">{title}</h2>
+            <p className="text-muted-foreground text-sm">{description}</p>
+          </div>
+          <Separator className="my-6" />
+        </>
+      ) : null}
+      <div className="flex flex-col lg:flex-row lg:space-x-6 lg:space-y-0">
+        <aside className="lg:w-1/5">
+          <SidebarSettings
+            items={sidebarNavItems}
+            getRoute={getRoute}
+            isActive={isActive}
+          />
+        </aside>
+        <div className="flex-1">{children}</div>
+      </div>
+    </div>
+  );
+};
