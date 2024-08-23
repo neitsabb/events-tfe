686fdc8d3a7b77df9f7156711ac473cef2d8e15a
BastienVdp <bastienpp@hotmail.com>
2024-08-23 13:00:26 +0200

    fix: put selected organization if isn't in the session

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Shared\Http\Middlewares\EnsureOrganizationSelected;
 use App\Shared\Http\Middlewares\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
+            EnsureOrganizationSelected::class,
             HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
