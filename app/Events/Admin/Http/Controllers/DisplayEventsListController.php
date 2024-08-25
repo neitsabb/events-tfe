e884c038c69dca7384012205703174d49d6a50ea
BastienVdp <bastienpp@hotmail.com>
2024-08-25 22:05:34 +0200

    clean: remove unecessary condition

--- app/Events/Admin/Http/Controllers/DisplayEventsListController.php
+++ app/Events/Admin/Http/Controllers/DisplayEventsListController.php
@@ -14,13 +14,9 @@ class DisplayEventsListController
      */
     public function __invoke(Request $request)
     {
-        $organisation = $request->session()->get('selected_organization')
-            ? $request->session()->get('selected_organization')
-            : $request->user()->organizations()->first();
-
         return Inertia::render('Events/Admin/Index/View', [
             'events' => EventResource::collection(
-                Event::where('organization_id', $organisation->id)
+                Event::where('organization_id', session('selected_organization')->id)
                     ->orderBy('start_date', 'desc')
                     ->get()
             ),
