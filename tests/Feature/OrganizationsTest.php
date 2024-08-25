2277ec87a764a9e1e166205f1f8bd312f12d2bf3
BastienVdp <bastienpp@hotmail.com>
2024-08-25 20:18:42 +0200

    tests: TDD / add users to organisation

---
+++ tests/Feature/OrganizationsTest.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Mail\InvitationMail;
+use App\Organization\Shared\Enums\OrganizationTypeEnum;
+use App\User\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Tests\TestCase;
+
+class OrganizationsTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_can_create_new_organisation(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->post(
+                route('organizations.store'),
+                [
+                    'name' => 'Organization Name',
+                    'description' => 'Organization Description',
+                    'type' => OrganizationTypeEnum::ASSOCIATION->value,
+                ]
+            );
+
+        // Assert: Vérifiez que l'utilisateur est redirigé 
+        $response->assertRedirect();
+
+        // Assert: Vérifiez que l'organisation a été créée
+        $this->assertDatabaseHas('organizations', [
+            'name' => 'Organization Name',
+            'description' => 'Organization Description',
+        ]);
+    }
+
+    public function test_should_not_create_new_organisation_with_invalid_data(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->post(
+                route('organizations.store'),
+                [
+                    'name' => '',
+                    'description' => '',
+                    'type' => OrganizationTypeEnum::ASSOCIATION->value,
+                ]
+            );
+
+        $response->assertSessionHasErrors(['name', 'description']);
+    }
+
+    public function test_can_select_organization(): void
+    {
+        $user = User::factory()->create();
+
+        $organization = $user->organizations()->create([
+            'name' => 'Organization Name',
+            'description' => 'Organization Description',
+            'type' => OrganizationTypeEnum::ASSOCIATION->value,
+        ]);
+
+        $response = $this->actingAs($user)
+            ->post(route('organizations.switch', $organization->id));
+
+        $response->assertSessionHas('selected_organization');
+
+        $this->assertEquals($organization->id, session('selected_organization')->id);
+    }
+
+    public function test_adds_existing_user_to_organization(): void
+    {
+        $user = User::factory()->create();
+
+        $organization = $user->organizations()->create([
+            "name" => 'Organization Name',
+            "description" => 'Organization Description',
+        ]);
+
+        $this->withSession(['selected_organization' => $organization]);
+
+        $existingUser = User::factory()->create();
+
+        $response =  $this
+            ->actingAs($user)
+            ->post(route('organizations.invite'), [
+                'email' => $existingUser->email,
+            ]);
+
+        $response->assertSessionHasNoErrors();
+        $response->assertCreated();
+
+        $this->assertDatabaseHas('organization_user', [
+            'organization_id' => $organization->id,
+            'user_id' => $existingUser->id,
+        ]);
+    }
+
+    public function test_adds_new_user_to_organization(): void
+    {
+        // Arrange
+        Mail::fake();
+
+        $email = 'bastienpp@hotmail.com';
+
+        $user = User::factory()->create();
+
+        $organization = $user->organizations()->create([
+            'name' => 'Organization Name',
+            'description' => 'Organization Description',
+        ]);
+
+        $this->withSession(['selected_organization' => $organization]);
+
+        $response = $this
+            ->actingAs($user)
+            ->post(route('organizations.invite'), [
+                'email' => $email,
+            ]);
+
+
+        $response->assertCreated();
+        $response->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas('users', ['email' => $email]);
+
+        $this->assertDatabaseHas('organization_user', [
+            'organization_id' => $organization->id,
+            'user_id' => User::where('email', $email)->firstOrFail()->id,
+        ]);
+
+        // Assert: Vérifiez qu'un email d'invitation a été envoyé
+        Mail::assertSent(InvitationMail::class, function ($mail) use ($email) {
+            return $mail->hasTo($email);
+        });
+    }
+
+    public function test_should_not_add_user_to_organization_if_already_in(): void
+    {
+        $user = User::factory()->create();
+
+        $organization = $user->organizations()->create([
+            'name' => 'Organization Name',
+            'description' => 'Organization Description',
+        ]);
+
+        $this->withSession(['selected_organization' => $organization]);
+
+
+        $this
+            ->actingAs($user)
+            ->post(route('organizations.invite'), [
+                'email' => $user->email,
+            ]);
+
+        $this->assertCount(1, $organization->users);
+    }
+
+    public function test_should_not_add_user_to_organization_if_email_is_invalid(): void
+    {
+        $user = User::factory()->create();
+
+        $organization = $user->organizations()->create([
+            'name' => 'Organization Name',
+            'description' => 'Organization Description',
+        ]);
+
+        $this->withSession(['selected_organization' => $organization]);
+
+        $response = $this
+            ->actingAs($user)
+            ->post(route('organizations.invite'), [
+                'email' => 'invalid-email',
+            ]);
+
+        $this->assertDatabaseCount('users', 1);
+
+        $response->assertSessionHasErrors(['email']);
+    }
+}
