d6098c86f6685eb6a4f8297d57ff757bb40794ed
BastienVdp <bastienpp@hotmail.com>
2024-08-25 22:24:06 +0200

    clean: delete unused files

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,17 +1,15 @@
-import "./bootstrap";
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-import { createRoot } from "react-dom/client";
-import { createInertiaApp } from "@inertiajs/react";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.tsx`,
-            import.meta.glob("./Pages/**/*.tsx")
+            import.meta.glob('./Pages/**/*.tsx')
         ),
     setup({ el, App, props }) {
         const root = createRoot(el);
@@ -19,6 +17,6 @@ createInertiaApp({
         root.render(<App {...props} />);
     },
     progress: {
-        color: "#4B5563",
+        color: '#4B5563',
     },
 });
