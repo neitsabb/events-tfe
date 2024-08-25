0c6b168e4a6ed59927e8e4d2d3fec3e66a5531ab
BastienVdp <bastienpp@hotmail.com>
2024-08-25 20:17:18 +0200

    refactor: use "actingAs"

--- tests/Feature/EventsTest.php
+++ tests/Feature/EventsTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Feature;
 
-use App\Events\Shared\Enums\EventStatusEnum;
 use Tests\TestCase;
+use App\Events\Shared\Models\Event;
 use Illuminate\Support\Facades\Auth;
+use App\Events\Shared\Enums\EventStatusEnum;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -16,69 +17,56 @@ class EventsTest extends TestCase
 	public function test_store_new_event_and_redirects()
 	{
 		$user = \App\User\Models\User::factory()->create();
-		Auth::login($user);
 
-		// Création d'une organisation associée à l'utilisateur
 		$organization = $user->organizations()->create([
 			"name" => 'Organization Name',
 			"description" => 'Organization Description',
 		]);
 
-		// Simulation de la session avec l'organisation sélectionnée
 		$this->withSession(['selected_organization' => $organization]);
 
 		$data = [
 			'name' => $this->faker->sentence,
 			'description' => $this->faker->paragraph,
-			// Note: 'organization_id' n'est plus nécessaire ici, car il est ajouté dans `prepareForValidation`.
 		];
 
-		// Exécution de la requête POST pour créer un événement
-		$response = $this->post(route('events.store'), $data);
+		$response = $this
+			->actingAs($user)
+			->post(route('events.store'), $data);
 
-		// Vérification du statut de redirection
 		$response->assertStatus(302);
 
-		// Récupération de l'événement créé dans la base de données
-		$event = \App\Events\Shared\Models\Event::where('name', $data['name'])->firstOrFail();
+		$event = Event::where('name', $data['name'])->firstOrFail();
 
-		// Vérification que la redirection utilise l'UUID correct
 		$response->assertRedirect(route('events.show', ['id' => $event->id]));
 
-		// Vérification que l'événement est bien présent dans la base de données
 		$this->assertDatabaseHas('events', [
 			'name' => $data['name'],
 			'description' => $data['description'],
-			'organization_id' => $organization->id,  // On vérifie aussi l'organisation associée
+			'organization_id' => $organization->id,
 		]);
 	}
 
 	/** @test */
 	public function test_configure_new_event_and_redirect_to_show_page()
 	{
-		// Créer un utilisateur et se connecter
 		$user = \App\User\Models\User::factory()->create();
-		Auth::login($user);
 
-		// Créer une organisation fictive pour le test
 		$organization = $user->organizations()->create([
 			"name" => 'Organization Name',
 			"description" => 'Organization Description',
 		]);
 
-		// Simuler la session avec l'organisation sélectionnée
 		$this->withSession(['selected_organization' => $organization]);
 
-		// Créer un événement fictif pour le test
 		$event = $organization->events()->create([
 			"name" => 'Event Name',
 			"description" => 'Event Description',
 		]);
 
-		// Créer des données de requête simulées
 		$data = [
-			'start_date' => now()->addDays(1)->format('Y-m-d H:i:s'), // Exemple de date future
-			'end_date' => now()->addDays(4)->format('Y-m-d H:i:s'),   // Exemple de date future
+			'start_date' => now()->addDays(1)->format('Y-m-d H:i:s'),
+			'end_date' => now()->addDays(4)->format('Y-m-d H:i:s'),
 			'location' => $this->faker->address,
 			'tickets' => [
 				['name' => 'General Admission', 'price' => 1000, 'quantity' => 10],
@@ -88,16 +76,14 @@ class EventsTest extends TestCase
 			],
 		];
 
-		// Simuler une requête POST vers le contrôleur avec les données simulées
-		$response = $this->post(route('events.configure', ['id' => $event->id]), $data);
+		$response = $this
+			->actingAs($user)
+			->post(route('events.configure', ['id' => $event->id]), $data);
 
-		// Assurez-vous que la redirection est correcte
 		$response->assertRedirect(route('events.show', ['id' => $event->id]));
 
-		// Recharger l'événement depuis la base de données après l'exécution de l'action
 		$event->refresh();
 
-		// Vérifiez que l'action ConfigureNewEventAction a correctement mis à jour l'événement
 		$this->assertEquals(EventStatusEnum::DRAFT->value, $event->status);
 		$this->assertEquals($event->name, "Event Name");
 		$this->assertEquals($event->description, "Event Description");
@@ -105,10 +91,8 @@ class EventsTest extends TestCase
 		$this->assertEquals($event->end_date->format('Y-m-d H:i:s'), $data['end_date']);
 		$this->assertEquals($event->location, $data['location']);
 
-		// Vérifiez que les tickets et extras ont été correctement créés
-		$this->assertCount(2, $event->tickets); // Assurez-vous que les tickets ont été créés
+		$this->assertCount(2, $event->tickets);
 
-		// Vérifiez les détails des tickets
 		$generalAdmission = $event->tickets->where('name', 'General Admission')->first();
 		$this->assertNotNull($generalAdmission);
 		$this->assertEquals(1000, $generalAdmission->price);
--- tests/Feature/TicketsTest.php
+++ tests/Feature/TicketsTest.php
@@ -10,85 +10,88 @@ use Tests\TestCase;
 
 class TicketsTest extends TestCase
 {
-	use RefreshDatabase, WithFaker;
+    use RefreshDatabase, WithFaker;
 
-	/** @test */
-	public function test_store_new_ticket_and_redirect_to_event_show_page()
-	{
-		$user = \App\User\Models\User::factory()->create();
-		Auth::login($user);
+    /** @test */
+    public function test_store_new_ticket_and_redirect_to_event_show_page()
+    {
+        $user = \App\User\Models\User::factory()->create();
+        Auth::login($user);
 
-		$organization = $user->organizations()->create([
-			"name" => 'Organization Name',
-			"description" => 'Organization Description',
-		]);
+        $organization = $user->organizations()->create([
+            "name" => 'Organization Name',
+            "description" => 'Organization Description',
+        ]);
 
-		$event = $organization->events()->create([
-			"name" => 'Event Name',
-			"description" => 'Event Description',
-			"organization_id" => $organization->id,
-		]);
+        $event = $organization->events()->create([
+            "name" => 'Event Name',
+            "description" => 'Event Description',
+            "organization_id" => $organization->id,
+        ]);
 
-		$requestData = [
-			"event_id" =>  $event->id,
-			"type" =>  "extra",
-			"name" =>  "Test Ticket",
-			"description" =>  "Test Ticket Description",
-			"quantity" =>  100,
-			"price" =>  19.99,
-		];
+        $requestData = [
+            "event_id" =>  $event->id,
+            "type" =>  "extra",
+            "name" =>  "Test Ticket",
+            "description" =>  "Test Ticket Description",
+            "quantity" =>  100,
+            "price" =>  19.99,
+        ];
 
-		$response = $this->post(route('events.tickets.store', ['id' => $event->id]), $requestData);
+        $response = $this->post(route('events.tickets.store', ['id' => $event->id]), $requestData);
 
-		$response->assertStatus(302);
+        $response->assertStatus(302);
 
-		$response->assertRedirect(route('events.show', ['id' => $event->id, 'panel' => 'tickets']));
+        $response->assertRedirect(route('events.show', ['id' => $event->id, 'panel' => 'tickets']));
 
-		$this->assertDatabaseHas('tickets', $requestData);
-	}
+        $this->assertDatabaseHas('tickets', $requestData);
+    }
 
-	/** @test */
-	public function test_update_ticket_and_redirect_to_event_show_page()
-	{
-		$user = \App\User\Models\User::factory()->create();
-		Auth::login($user);
+    /** @test */
+    public function test_update_ticket_and_redirect_to_event_show_page()
+    {
+        $user = \App\User\Models\User::factory()->create();
 
-		$organization = $user->organizations()->create([
-			"name" => 'Organization Name',
-			"description" => 'Organization Description',
-		]);
+        $event = $user
+            ->organizations()
+            ->create([
+                "name" => 'Organization Name',
+                "description" => 'Organization Description',
+            ])
 
-		$event = $organization->events()->create([
-			"name" => 'Event Name',
-			"description" => 'Event Description',
-		]);
+            ->events()
+            ->create([
+                "name" => 'Event Name',
+                "description" => 'Event Description',
+            ]);
 
-		$ticket = $event->tickets()->create([
-			'name' => 'Original Ticket',
-			'type' => TicketTypeEnum::ADMISSION->value,
-			'description' => 'Original Description',
-			'quantity' => 50,
-			'price' => 10.00,
-		]);
+        $ticket = $event->tickets()->create([
+            'name' => 'Original Ticket',
+            'type' => TicketTypeEnum::ADMISSION->value,
+            'description' => 'Original Description',
+            'quantity' => 50,
+            'price' => 10.00,
+        ]);
 
-		$data = [
-			'ticket_id' => $ticket->id,
-			'type' => TicketTypeEnum::ADMISSION->value,
-			'name' => 'Updated Ticket Name',
-			'description' => 'Updated Ticket Description',
-			'quantity' => 100,
-			'price' => 20.00,
-		];
 
-		$response = $this->post(route('events.tickets.update', ['id' => $event->id]), $data);
+        $response = $this
+            ->actingAs($user)
+            ->post(route('events.tickets.update', $event->id), [
+                'ticket_id' => $ticket->id,
+                'type' => TicketTypeEnum::ADMISSION->value,
+                'name' => 'Updated Ticket Name',
+                'description' => 'Updated Ticket Description',
+                'quantity' => 100,
+                'price' => 20.00,
+            ]);
 
-		$response->assertRedirect(route('events.show', ['id' => $event->id, 'panel' => 'tickets']));
+        $response->assertRedirect(route('events.show', ['id' => $event->id, 'panel' => 'tickets']));
 
-		$ticket->refresh();
+        $ticket->refresh();
 
-		$this->assertEquals('Updated Ticket Name', $ticket->name);
-		$this->assertEquals('Updated Ticket Description', $ticket->description);
-		$this->assertEquals(100, $ticket->quantity);
-		$this->assertEquals(20.00, $ticket->price);
-	}
+        $this->assertEquals('Updated Ticket Name', $ticket->name);
+        $this->assertEquals('Updated Ticket Description', $ticket->description);
+        $this->assertEquals(100, $ticket->quantity);
+        $this->assertEquals(20.00, $ticket->price);
+    }
 }
