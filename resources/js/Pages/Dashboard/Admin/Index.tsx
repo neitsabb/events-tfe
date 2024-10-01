86f73f60249c83b297059eebe421940922bb4539
BastienVdp <bastienpp@hotmail.com>
2024-10-01 14:57:15 +0200

    feat: integrate stripe connect feature

--- resources/js/Pages/Dashboard/Admin/Index.tsx
+++ resources/js/Pages/Dashboard/Admin/Index.tsx
@@ -3,14 +3,30 @@ import { Button } from '@/Components/ui/button';
 import { DatePickerWithRange } from '@/Components/ui/datepicker';
 import { Tabs, TabsList, TabsTrigger } from '@/Components/ui/tabs';
 import AuthenticatedLayout from '@/Layouts/Admin/AuthenticatedLayout';
-import { Head, Link } from '@inertiajs/react';
+import { Head, Link, useForm } from '@inertiajs/react';
 import { Notifications } from './Partials/Notifications';
 import { Overview } from './Partials/Overview';
 import { Statistiques } from './Partials/Statistiques';
 import { Alert, AlertDescription, AlertTitle } from '@/Components/ui/alert';
 import { MessageSquareWarningIcon } from 'lucide-react';
+import { useState } from 'react';
 
 export default function Dashboard() {
+    const [isLoading, setIsLoading] = useState(false);
+
+    // useForm pour gérer le formulaire avec Inertia
+    const { post } = useForm();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        // Utilisation de Inertia pour soumettre la requête
+        post(route('organizations.stripe.connect'), {
+            onFinish: () => setIsLoading(false), // Fin du loading après la requête
+            onError: () => setIsLoading(false), // Fin du loading en cas d'erreur
+        });
+    };
     return (
         <AuthenticatedLayout className="space-y-6">
             <Head title="Dashboard" />
@@ -27,22 +43,39 @@ export default function Dashboard() {
             <Alert>
                 <MessageSquareWarningIcon className="h-6 w-6" />
                 <AlertTitle>
-                    Complétez votre organisation pour publier votre premier
-                    événement
+                    Complétez votre organisation pour vendre des billets
                 </AlertTitle>
                 <AlertDescription>
-                    Nous avons besoin de quelques informations supplémentaires
-                    pour que vous puissiez commencer à créer des événements et
-                    vendre des billets.
+                    Notre fournisseur de services de paiement a besoin des
+                    détails de votre organisation pour que vous perceviez vos
+                    revenus.
                 </AlertDescription>
-                <Link
-                    href={route('organizations.settings')}
+                <Button
+                    onClick={handleClick}
+                    variant={'ghost'}
                     className={
-                        'underline underline-offset-4 block ml-10 mt-4 whitespace-nowrap text-sm font-medium text-primary/90 hover:text-primary'
+                        'underline underline-offset-4  ml-7 mt-4 whitespace-nowrap text-sm font-medium text-primary/90 hover:text-primary flex gap-2 disabled:text-accent-foreground'
                     }
+                    disabled={isLoading} // Désactiver le bouton pendant le chargement
                 >
-                    Aller aux paramètres de l'organisation
-                </Link>
+                    {isLoading && (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className={'animate-spin w-4'}
+                        >
+                            <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+                        </svg>
+                    )}
+                    Compléter mon compte Stripe
+                </Button>
             </Alert>
             <Tabs defaultValue="overview">
                 <TabsList>
