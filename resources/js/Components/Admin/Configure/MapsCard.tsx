c8f6fb96f1935aa1028a0c27bb56d4d7b494af56
BastienVdp <bastienpp@hotmail.com>
2024-10-22 13:28:52 +0200

    refactor: add type button

--- resources/js/Components/Admin/Configure/MapsCard.tsx
+++ resources/js/Components/Admin/Configure/MapsCard.tsx
@@ -1,29 +1,29 @@
-import { CoordsProps } from "@/types";
+import { CoordsProps } from '@/types';
 import {
     AdvancedMarker,
     APIProvider,
     Map,
     Pin,
-} from "@vis.gl/react-google-maps";
-import { Button } from "@/Components/ui/button";
+} from '@vis.gl/react-google-maps';
+import { Button } from '@/Components/ui/button';
 
 export const MapsCard = ({
     value,
     coords,
     cancelSelection,
 }: {
-    value: string;
+    value: string | undefined;
     coords: CoordsProps;
     cancelSelection: () => void;
 }) => {
     return (
         <div className="bg-gray-100 text-card-foreground border border-border  rounded-md">
-            <APIProvider apiKey={"AIzaSyCBSL2QY5gvl7EiXFTs-K2R1rQ6qrbEN5E"}>
+            <APIProvider apiKey={'AIzaSyCBSL2QY5gvl7EiXFTs-K2R1rQ6qrbEN5E'}>
                 <Map
                     style={{
-                        width: "100%",
-                        height: "148px",
-                        borderRadius: "16px",
+                        width: '100%',
+                        height: '148px',
+                        borderRadius: '16px',
                     }}
                     zoom={16}
                     center={{ lat: coords?.lat, lng: coords?.lng }}
@@ -39,7 +39,11 @@ export const MapsCard = ({
             </APIProvider>
             <div className="flex flex-col items-center space-y-4 px-4 py-6 bg-white rounded-bl-md rounded-br-md">
                 <span className="text-sm max-w-sm font-medium">{value}</span>
-                <Button variant="outline" onClick={cancelSelection}>
+                <Button
+                    type="button"
+                    variant="outline"
+                    onClick={cancelSelection}
+                >
                     Changer de localisation
                 </Button>
             </div>
