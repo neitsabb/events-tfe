b887aa996c389b41522269fdf5197dcc477addb4
BastienVdp <bastienpp@hotmail.com>
2024-10-23 16:01:50 +0200

    styles: reduce gap between label / input

--- resources/js/Components/Admin/Field.tsx
+++ resources/js/Components/Admin/Field.tsx
@@ -3,34 +3,36 @@ import { cn } from '@/utils';
 import { Label } from '../ui/label';
 
 interface FieldProps {
-  label: string;
-  id: string;
-  children: React.ReactNode;
-  className?: string;
-  errors?: ErrorsProps;
-  required?: boolean;
+    label: string;
+    id: string;
+    children: React.ReactNode;
+    className?: string;
+    errors?: ErrorsProps;
+    required?: boolean;
 }
 
 export const Field: React.FC<FieldProps> = ({
-  label,
-  id,
-  children,
-  className,
-  errors,
-  required = true,
+    label,
+    id,
+    children,
+    className,
+    errors,
+    required = true,
 }) => {
-  return (
-    <div className={cn('w-full flex flex-col gap-3', className)}>
-      <Label htmlFor={id} className="flex gap-1 items-center">
-        {label}{' '}
-        {required && (
-          <span className="text-xs font-light text-destructive">*</span>
-        )}
-      </Label>
-      {children}
-      {errors && errors[id] && (
-        <p className="text-xs text-red-500">{errors[id]}</p>
-      )}
-    </div>
-  );
+    return (
+        <div className={cn('w-full flex flex-col gap-2', className)}>
+            <Label htmlFor={id} className="flex gap-1 items-center">
+                {label}{' '}
+                {required && (
+                    <span className="text-xs font-light text-destructive">
+                        *
+                    </span>
+                )}
+            </Label>
+            {children}
+            {errors && errors[id] && (
+                <p className="text-xs text-red-500">{errors[id]}</p>
+            )}
+        </div>
+    );
 };
