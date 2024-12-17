c630d1c9015486128de011c0ca56e520b4c7569c
BastienVdp <bastienpp@hotmail.com>
2024-12-18 00:46:58 +0100

    build: prepare for prod

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: "resources/js/app.tsx",
+            input: ['resources/js/app.tsx', 'resources/js/app.css'],
             refresh: true,
         }),
         react(),
     ],
     optimizeDeps: {
-        exclude: ["react-colorful"],
+        exclude: ['react-colorful'],
+    },
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            input: ['resources/js/app.tsx', 'resources/js/app.css'],
+        },
     },
 });
