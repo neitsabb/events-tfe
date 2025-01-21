5a065204f1a5b471c196de0deab9f87f60d9cab5
BastienVdp <bastienpp@hotmail.com>
2025-01-21 17:35:53 +0100

    build production

---
+++ public/build/assets/Error-DjxQmE35.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-DXmYKjII.js";import{c as o}from"./button-LZ4wRFbW.js";const m=({message:r,className:s})=>t.jsx("div",{className:o("text-xs text-red-400",s),children:r});export{m as E};
--- public/build/assets/View-BdTa6JaU.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-FSxDIhYd.js";const i=()=>e.jsx("div",{children:"Paiement failed"});export{i as default};
